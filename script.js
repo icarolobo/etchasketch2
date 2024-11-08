d0e5e6038948fc6a38f7ceb1ba4c1bca635ceac1
Icaro <icaropessoalobo@gmail.com>
2024-11-08 10:45:35 -0300

    Add initial logic for grid size user input

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const parent = document.querySelector(".container");
+const resetButton = document.querySelector(".resetButton");
+let sizeNewGrid = 16;
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    parent.appendChild(div);
-    div.classList.add("gridDiv");
+resetButton.addEventListener("click", askSize);
+
+function askSize() {
+    sizeNewGrid = prompt("Size of the new grid:");
+    deleteChild();
+    newGrid();    
 }
 
-const childrenDiv = document.querySelectorAll(".gridDiv");
+function deleteChild() {
+    while (parent.firstChild) {
+        parent.removeChild(parent.lastChild);
+    }
+}
+
+function newGrid() {
+    for (let i = 0; i < (sizeNewGrid * sizeNewGrid); i++) {
+        const div = document.createElement("div");
+        parent.appendChild(div);
+        div.classList.add("gridDiv");
+    }
+    const childrenDiv = document.querySelectorAll(".gridDiv");
+    Array.from(childrenDiv).forEach(gridDiv => {
+        gridDiv.addEventListener("mouseover", toggleBackground);
+    });
+}
+
+newGrid();
 
 function toggleBackground(e) {
     e.target.classList.toggle("black");
 };
-
-Array.from(childrenDiv).forEach(gridDiv => {
-    gridDiv.addEventListener("mouseover", toggleBackground);
-});
